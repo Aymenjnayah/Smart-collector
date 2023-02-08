92bc9cd7f390026e2bf6cdc18d52df4f3f39c697
aymen jnayah <contactaymenjnayah@gmail.com>
2023-02-08 20:07:28 +0000

    adding routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,10 @@
+import 'package:smart_collector/views/onboarding.dart';
+
+class AppRoutes {
+  static const splash = '/splash';
+    static const Onboarding = '/onboarding';
+
+  static const login = '/login';
+  static const register = '/register';
+  static const dashboard = '/dashboard';
+}
