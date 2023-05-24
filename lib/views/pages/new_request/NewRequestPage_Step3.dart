43fdd88774b75fbbb1c3eba5062ab9f860b2ccd9
Moussa Riadh <riadh.moussa@groupe-telnet.net>
2023-05-24 15:47:11 +0100

    fix issues scrolling

--- lib/views/pages/new_request/NewRequestPage_Step3.dart
+++ lib/views/pages/new_request/NewRequestPage_Step3.dart
@@ -17,84 +17,86 @@ class NewRequestPageStepThree extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const CustumAppBar(
-              title: "Request details",
-            ),
-            const SizedBox(height: 20),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.green),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const CustumAppBar(
+                title: "Request details",
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Obx(() => Column(
-                    children: controller.items.value.map((item) => Column(
-                      children: [
-                        Image.network(
-                          item.imageUrl,
-                          height: 200,
-                          fit: BoxFit.cover,
-                        ),
-                        SizedBox(height: 10),
-                        Center(
-                          child: Text(
-                            item.title,
-                            style: AppStyles.neutralBlack16W7Style,
-                          ),
-                        ),
-                      ],
-                    )).toList(),
-                  )),
-                  const SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Obx(
-                          () => Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+              const SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.green),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Obx(() => Column(
+                      children: controller.items.value.map((item) => Column(
                         children: [
-                          const SizedBox(height: 10),
-                          Text("Amount (L)", style: AppStyles.black14W7Style),
-                          Text(controller.request.value.amount.toString()),
-                          Text("Address", style: AppStyles.black14W7Style),
-                          Text(controller.request.value.address.toString()),
-                          Text("Appointment", style: AppStyles.black14W7Style),
-                          Text(controller.request.value.date.toString()),
-                          const SizedBox(height: 10),
+                          Image.network(
+                            item.imageUrl,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                          SizedBox(height: 10),
+                          Center(
+                            child: Text(
+                              item.title,
+                              style: AppStyles.neutralBlack16W7Style,
+                            ),
+                          ),
                         ],
+                      )).toList(),
+                    )),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Obx(
+                            () => Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 10),
+                            Text("Amount (L)", style: AppStyles.black14W7Style),
+                            Text(controller.request.value.amount.toString()),
+                            Text("Address", style: AppStyles.black14W7Style),
+                            Text(controller.request.value.address.toString()),
+                            Text("Appointment", style: AppStyles.black14W7Style),
+                            Text(controller.request.value.date.toString()),
+                            const SizedBox(height: 10),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  const Divider(
-                    height: 1,
-                    thickness: 1,
-                    color: Color(0xFFE9F7EF),
-                  ),
-                  const SizedBox(height: 10),
-                  Center(
-                    child: QrImageView(
-                      data: controller.request.value.docId.toString(),
-                      version: QrVersions.auto,
-                      size: 150,
-                      gapless: false,
+                    const Divider(
+                      height: 1,
+                      thickness: 1,
+                      color: Color(0xFFE9F7EF),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  SizedBox(
-                    width: double.infinity,
-                    child: SubmitButton(buttonText: "Go Home",onPressed: ()=>{
-                      Get.offAllNamed(AppRoutes.dashboard)
-                    }),
-                  ),
-                ],
+                    const SizedBox(height: 10),
+                    Center(
+                      child: QrImageView(
+                        data: controller.request.value.docId.toString(),
+                        version: QrVersions.auto,
+                        size: 150,
+                        gapless: false,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: SubmitButton(buttonText: "Go Home",onPressed: ()=>{
+                        Get.offAllNamed(AppRoutes.dashboard)
+                      }),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
